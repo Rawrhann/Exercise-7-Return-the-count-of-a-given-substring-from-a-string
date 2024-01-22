f073b18a2cddf509a98f7e3e8a51a126f24a31d0
Ching <marcrohannmching@iskolarngbayan.pup.edu.ph>
2024-01-23 00:15:13 +0800

    finished code

--- exercise 7.py
+++ exercise 7.py
@@ -1,6 +1,8 @@
 #Exercise 7: Return the count of a given substring from a string
 #Write a program to find how many times substring “Emma” appears in the given string.
 
-
 string = str("Marc is the good at playing video games. Marc is very lazy")
+
+count = string.count("Marc")
+print("Marc appeared", count, "times")
 
